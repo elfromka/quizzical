41734b40b434365d2f2b4e38fa7326be9e9affac
Orsi Sebestyen <elfromka@gmail.com>
2022-07-21 22:58:13 +0300

    Uploading some styles for NotFound page as well

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-    return <div>NotFound</div>;
+    return (
+        <div className="not-found container">
+            <h1 className="not-found__title not-found__title--dark-grey">
+                404 - Not found
+            </h1>
+            <h2 className="not-found__subtitle not-found__subtitle--primary">
+                The page you requested was not found on this server.
+            </h2>
+            <Link className="btn btn--primary btn--xl" to="/">
+                Go back
+            </Link>
+        </div>
+    );
 };
 
 export default NotFound;
